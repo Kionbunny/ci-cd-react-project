7b4bd5f02fbf41ef4dc9e14a04c76e3b40d35794
ajay <mashapoguajay@gmail.com>
2025-09-22 23:01:23 +0530

    Emoji is added in App.js file

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React & CI/CD with Jenkins 👉
+          Learn React & CI/CD with Jenkins 👉 🎉✌️
         </a>
       </header>
     </div>
