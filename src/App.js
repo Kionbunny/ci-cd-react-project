1413008a95fd6f49ebf0f58ddc4517e04962cd62
ajay <mashapoguajay@gmail.com>
2025-09-22 22:30:06 +0530

    App.js modified

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React & CI/CD with Jenkins
         </a>
       </header>
     </div>
